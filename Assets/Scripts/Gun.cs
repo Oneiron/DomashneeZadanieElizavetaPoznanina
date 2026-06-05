a4a9219ca7eb172555f9cf7697a47d501e1a9102
Oneiron <82400714+Oneiron@users.noreply.github.com>
2026-06-05 20:33:57 +0300

    new6

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,29 +4,18 @@ namespace Lesson
 {
     public sealed class Gun : Weapon
     {
+        [SerializeField] private int _countInClip = 1000;
         [SerializeField] private Bullet _bulletPrefab;
 
         private Transform _bulletRoot;
         private Bullet[] _bullets;
 
-        private void Start()
+        protected override void Start()
         {
-            _bulletRoot = new GameObject("Bullet root").transform;
+            base.Start();
+
+            _bulletRoot = new GameObject("BulletRoot").transform;
             Recharge();
-        }
-
-        public override void Fire()
-        {
-            if (CanShoot == false)
-            {
-                return;
-            }
-
-            if (TryGetBullet(out Bullet bullet))
-            {
-                bullet.Run(_barrel.forward * _force, _barrel.position);
-                LastShootTime = 0.0f;
-            }
         }
 
         public override void Recharge()
@@ -63,24 +52,38 @@ namespace Lesson
 
                 if (bullet.IsActive)
                 {
-                    return false;
+                    return true;
                 }
             }
 
-            return true;
+            return false;
+        }
+
+        public override void Fire()
+        {
+            if (CanShoot == false)
+            {
+                return;
+            }
+
+            if (TryGetBullet(out Bullet bullet))
+            {
+                bullet.Run(_barrel.forward * Force, _barrel.position);
+                LastShootTime = 0.0f;
+            }
         }
 
         private bool TryGetBullet(out Bullet result)
         {
             int candidate = -1;
-            result = default;
 
             if (_bullets == null)
             {
+                result = default;
                 return false;
             }
 
-            for (int i = 0; i < _bullets.Length; i++)
+            for (var i = 0; i < _bullets.Length; i++)
             {
                 Bullet bullet = _bullets[i];
                 if (bullet == null)
@@ -99,6 +102,7 @@ namespace Lesson
 
             if (candidate == -1)
             {
+                result = default;
                 return false;
             }
 
@@ -114,3 +118,4 @@ namespace Lesson
         }
     }
 }
+
